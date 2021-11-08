53a22e810d2bbcc8880f75788ab7387a1a8d37d9
stefanorosss <60915557+stefanorosss@users.noreply.github.com>
2021-11-08 19:00:22 +0100

    Add path variable
    Added ability to use more than 2 images at a time by loading a whole folder

--- inference_img.py
+++ inference_img.py
@@ -3,6 +3,7 @@ import cv2
 import torch
 import argparse
 from torch.nn import functional as F
+from natsort import natsorted
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -13,7 +14,8 @@ if torch.cuda.is_available():
     torch.backends.cudnn.benchmark = True
 
 parser = argparse.ArgumentParser(description='Interpolation for a pair of images')
-parser.add_argument('--img', dest='img', nargs=2, required=True)
+parser.add_argument('--img', default = ['',''], dest='img', nargs=2, required=False)
+parser.add_argument('--path', default = False, help='path containing the images to be interpolated ')
 parser.add_argument('--exp', default=4, type=int)
 parser.add_argument('--ratio', default=0, type=float, help='inference ratio between two images with 0 - 1 range')
 parser.add_argument('--rthreshold', default=0.02, type=float, help='returns image when actual ratio falls in given range threshold')
@@ -24,7 +26,7 @@ args = parser.parse_args()
 
 try:
     try:
-        from model.RIFE_HDv2 import Model
+        from model.oldmodel.RIFE_HDv2 import Model
         model = Model()
         model.load_model(args.modelDir, -1)
         print("Loaded v2.x HD model.")
@@ -47,19 +49,32 @@ if args.img[0].endswith('.exr') and args.img[1].endswith('.exr'):
     img0 = (torch.tensor(img0.transpose(2, 0, 1)).to(device)).unsqueeze(0)
     img1 = (torch.tensor(img1.transpose(2, 0, 1)).to(device)).unsqueeze(0)
 
-else:
+elif args.img[0] != '':
     img0 = cv2.imread(args.img[0], cv2.IMREAD_UNCHANGED)
     img1 = cv2.imread(args.img[1], cv2.IMREAD_UNCHANGED)
     img0 = (torch.tensor(img0.transpose(2, 0, 1)).to(device) / 255.).unsqueeze(0)
     img1 = (torch.tensor(img1.transpose(2, 0, 1)).to(device) / 255.).unsqueeze(0)
+elif args.path:
+    img_paths = natsorted(os.listdir(args.path))
+    imgs = [cv2.imread(os.path.join(args.path,img_path), cv2.IMREAD_UNCHANGED) for img_path in img_paths]
 
-n, c, h, w = img0.shape
-ph = ((h - 1) // 32 + 1) * 32
-pw = ((w - 1) // 32 + 1) * 32
-padding = (0, pw - w, 0, ph - h)
-img0 = F.pad(img0, padding)
-img1 = F.pad(img1, padding)
+    imgs = [(torch.tensor(img.transpose(2, 0, 1)).to(device) / 255.).unsqueeze(0) for img in imgs ]
+else:
+    raise NameError('Add either img or path to the options passed to the script')
 
+if not args.path:
+    n, c, h, w = img0.shape
+    ph = ((h - 1) // 32 + 1) * 32
+    pw = ((w - 1) // 32 + 1) * 32
+    padding = (0, pw - w, 0, ph - h)
+    img0 = F.pad(img0, padding)
+    img1 = F.pad(img1, padding)
+else:
+    n, c, h, w = imgs[0].shape
+    ph = ((h - 1) // 32 + 1) * 32
+    pw = ((w - 1) // 32 + 1) * 32
+    padding = (0, pw - w, 0, ph - h)
+    imgs = [F.pad(img, padding) for img in imgs]
 
 if args.ratio:
     img_list = [img0]
@@ -85,7 +100,7 @@ if args.ratio:
                 img1_ratio = middle_ratio
     img_list.append(middle)
     img_list.append(img1)
-else:
+elif not args.path:
     img_list = [img0, img1]
     for i in range(args.exp):
         tmp = []
@@ -95,7 +110,20 @@ else:
             tmp.append(mid)
         tmp.append(img1)
         img_list = tmp
-
+else:
+    outer_list = []
+    for k in range(len(imgs)-1):
+        img_list = [imgs[k], imgs[k+1]]
+        for i in range(args.exp):
+            tmp = []
+            for j in range(len(img_list) - 1):
+                mid = model.inference(img_list[j], img_list[j + 1])
+                tmp.append(img_list[j])
+                tmp.append(mid)
+            tmp.append(img_list[j])
+            img_list = tmp
+        outer_list.extend(img_list)
+    img_list = outer_list
 if not os.path.exists('output'):
     os.mkdir('output')
 for i in range(len(img_list)):
